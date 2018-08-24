5c2bc87abb2209231c60b83786dce784f8360b47
Gabriel San Martin <gabrielsanmartin1275@gmail.com>
2018-08-24 00:07:01 -0400

    ProfessorController

--- src/main/java/br/com/gsm/smartplan/smartplanapi/controller/ProfessorController.java
+++ src/main/java/br/com/gsm/smartplan/smartplanapi/controller/ProfessorController.java
@@ -5,13 +5,16 @@
  */
 package br.com.gsm.smartplan.smartplanapi.controller;
 
+import br.com.gsm.smartplan.smartplanapi.exception.ResourceNotFoundException;
 import br.com.gsm.smartplan.smartplanapi.model.Professor;
 import br.com.gsm.smartplan.smartplanapi.repository.ProfessorRepository;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Gabriel San Martin
  */
+
 @RestController
 @RequestMapping("/professores")
 public class ProfessorController {
@@ -43,4 +47,43 @@ public class ProfessorController {
     public ResponseEntity<?> getById(@PathVariable("id") Long id) {
         return new ResponseEntity<>(professorRepository.findById(id), HttpStatus.OK);
     }
+    
+    //Retorna todos os professores.
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> listOfProfessores() {
+        return new ResponseEntity<>(professorRepository.findAll(), HttpStatus.OK);
+    }
+    
+    //Cria um professor
+    @RequestMapping(method = RequestMethod.POST, path="/cadastrar")
+    public Professor insert(@Valid @RequestBody Professor professor) {
+        return professorRepository.save(professor);
+    }
+    
+    //Atualiza um determinado professor
+    @RequestMapping(method = RequestMethod.PUT, path="/{id}")
+    public Professor updateConceito(@PathVariable("id") Long professor_id,
+            @Valid @RequestBody Professor professor_details) {
+
+        Professor professor = professorRepository.findById(professor_id)
+                .orElseThrow(() -> new ResourceNotFoundException("Professor", "professor", professor_id));
+
+        professor.setNome(professor_details.getNome());
+        professor.setEmail(professor_details.getEmail());
+        professor.setSenha(professor_details.getSenha());
+
+        Professor professorUpdate = professorRepository.save(professor);
+        return professorUpdate;
+    }
+    
+    @RequestMapping(method = RequestMethod.DELETE, path="/{id}")
+    public ResponseEntity<?> deleteProfessor(@PathVariable("id") Long professor_id) {
+        Professor professor = professorRepository.findById(professor_id)
+                .orElseThrow(() -> new ResourceNotFoundException("Professor", "id", professor_id));
+
+        professorRepository.delete(professor);
+
+        return ResponseEntity.ok().build();
+    }
+    
 }
